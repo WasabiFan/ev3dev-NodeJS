f4f32c1e6185cbcbd0584bb167b92589a3edc32a
WasabiFan <WasabiFan@outlook.com>
2014-06-28 10:51:09 -0700

    Initial commit. Includes basic infrastructure and reading of motor properties.

---
+++ EV3Base.ts
@@ -0,0 +1,65 @@
+﻿/// <reference path="node.d.ts"/>
+var fs = require("fs");
+var path = require('path');
+
+module FilePathConstructor {
+    var motorDeviceDir: string = '/sys/class/tacho-motor/';
+    var motorDirName: string = 'out{0}:motor:tacho';
+
+    export function motor(port: MotorPort) {
+        return path.join(motorDeviceDir, motorDirName.format(MotorPort[port]), '/');
+    }
+
+    export function motorProperty(port: MotorPort, property: MotorProperty) {
+        return path.join(motor(port), MotorProperty[property]);
+    }
+}
+
+interface String {
+    format(...args: any[]): string
+}
+
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+            ;
+    });
+};
+
+enum MotorPort {
+    A, B, C, D
+}
+
+enum MotorProperty {
+    device,
+    state,
+    target_speed,
+    direction,
+    stop_mode,
+    target_steer,
+    mode,
+    subsystem,
+    target_tacho,
+    power,
+    tacho,
+    target_time,
+    ramp_mode,
+    tacho_mode,
+    target_total_count,
+    regulation_mode,
+    target_power,
+    time,
+    run,
+    target_ramp_down_count,
+    type,
+    speed,
+    target_ramp_up_count,
+    uevent
+}
+
+module.exports.FilePathConstructor = FilePathConstructor;
+module.exports.MotorPort = MotorPort;
+module.exports.MotorProperty = MotorProperty;
---
+++ Motor.ts
@@ -0,0 +1,34 @@
+﻿/// <reference path="node.d.ts"/>
+/// <reference path="EV3Base.ts"/>
+
+var fs = require("fs");
+var base = require("./EV3Base.js");
+
+var FilePathConstructor = base.FilePathConstructor;
+var MotorPort = base.MotorPort;
+var MotorProperty = base.MotorProperty;
+
+class Motor {
+    private port: MotorPort
+
+    get speed(): number {
+        return parseInt(this.readProperty(MotorProperty.speed));
+    }
+
+    get position(): number {
+        var propertyPath: string = FilePathConstructor.motorProperty(this.port, MotorProperty.position);
+        return parseInt(fs.readFileSync(propertyPath).toString());
+    }
+
+    private readProperty(property: MotorProperty): string {
+        var propertyPath: string = FilePathConstructor.motorProperty(this.port, property);
+        return fs.readFileSync(propertyPath).toString().match(/[0-9]+/)[0];
+    }
+
+    constructor(port: MotorPort) {
+        this.port = port;
+    }
+}
+
+module.exports = Motor;
+module.exports.MotorPort = base.MotorPort;
