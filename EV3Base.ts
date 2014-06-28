c4c9ba79ea4431e383403e197d9816e8186ad705
WasabiFan <WasabiFan@outlook.com>
2014-06-28 14:45:56 -0700

    Added unlimited-mode motor control, doesn't include break/hold modes

--- EV3Base.ts
+++ EV3Base.ts
@@ -29,36 +29,64 @@ String.prototype.format = function () {
     });
 };
 
+function softBoolean(value: any, falseValue?: any, trueValue?: any) : boolean{
+    switch (value) {
+        case 0:        
+        case 'off':
+        case false:
+            return falseValue == undefined? false : falseValue;
+
+        case 1:
+        case 'on':
+        case true:
+            return trueValue == undefined? true : trueValue;
+
+        default:
+            return undefined;
+    }
+}
+
 enum MotorPort {
     A, B, C, D
 }
 
 enum MotorProperty {
-    device,
-    state,
-    target_speed,
-    direction,
-    stop_mode,
-    target_steer,
-    mode,
+    brake_mode,
+    hold_mode,
+    position,
+    position_setpoint,
+    pulses_per_second,
+    ramp_up,
+    reset,
+    run_mode,
+    speed_setpoint,
     subsystem,
-    target_tacho,
-    power,
-    tacho,
-    target_time,
-    ramp_mode,
-    tacho_mode,
-    target_total_count,
-    regulation_mode,
-    target_power,
-    time,
-    run,
-    target_ramp_down_count,
     type,
+    device,
+    polarity_mode,
+    position_mode,
+    power,
+    ramp_down,
+    regulation_mode,
+    run,
     speed,
-    target_ramp_up_count,
-    uevent,
-    position
+    state,
+    time_setpoint,
+    uevent
+}
+
+var MotorPropertyValidation = {
+    0: { type: 'string', values: ['on', 'off'] }, //brake_mode
+    1: { type: 'string', values: ['on', 'off'] }, //hold_mode
+    2: { type: 'number', min: -2147483648, max: 2147483648 }, //position
+    7: { type: 'string', values: ['forever', 'time', 'position'] }, //run_mode
+    8: { type: 'number', min: -100, max: 100 }, //speed_setpoint
+    10: { type: 'string', values: ['tacho', 'minitacho'] }, //type
+    14: { type: 'number', min: -100, max: 100 }, //power
+    16: { type: 'string', values: ['on', 'off'] }, //regulation_mode
+    17: { type: 'number', values: [0, 1] }, //run
+    18: { type: 'number', min: -100, max: 100 }, //speed
+
 }
 
 enum MotorType {
@@ -70,3 +98,5 @@ module.exports.FilePathConstructor = FilePathConstructor;
 module.exports.MotorPort = MotorPort;
 module.exports.MotorProperty = MotorProperty;
 module.exports.MotorType = MotorType;
+module.exports.MotorPropertyValidation = MotorPropertyValidation;
+module.exports.softBoolean = softBoolean;
--- Motor.ts
+++ Motor.ts
@@ -7,11 +7,14 @@ var base = require("./EV3Base.js");
 var FilePathConstructor = base.FilePathConstructor;
 var MotorPort = base.MotorPort;
 var MotorProperty = base.MotorProperty;
+var MotorPropertyValidation = base.MotorPropertyValidation;
 var MotorType = base.MotorType;
+var softBoolean = base.softBoolean;
 
 class Motor {
     private port: MotorPort
 
+    //Read-only properties
     get speed(): number {
         return parseInt(this.readProperty(MotorProperty.speed));
     }
@@ -20,11 +23,15 @@ class Motor {
         return parseInt(this.readProperty(MotorProperty.position));
     }
 
+    get power(): number {
+        return parseInt(this.readProperty(MotorProperty.power));
+    }
+
     get type(): string {
         return this.readProperty(MotorProperty.type);
     }
 
-    private readProperty(property: MotorProperty): string {        
+    private readProperty(property: MotorProperty): string {
         var propertyPath: string = FilePathConstructor.motorProperty(this.port, property);
         if (fs.existsSync(propertyPath))
             return fs.readFileSync(propertyPath).toString().match(/[0-9A-Za-z._]+/)[0];
@@ -32,9 +39,79 @@ class Motor {
             throw new Error('The property file could not be found. Either the specified motor is not available or the property does not exist.');
     }
 
+    //Writable properties
+
+    get targetSpeed(): number {
+        return parseInt(this.readProperty(MotorProperty.speed_setpoint));
+    }
+
+    set targetSpeed(value: number) {
+        this.writeProperty(MotorProperty.speed_setpoint, value);
+    }
+
+    private writeProperty(property: MotorProperty, value: any) {
+        var propertySpec = MotorPropertyValidation[property];
+
+        switch (propertySpec.type) {
+            case 'number':
+                if (isNaN(value))
+                    throw new Error('The specified value is not a number.');
+
+                if (typeof propertySpec.min != 'undefined')
+                    if (value < propertySpec.min)
+                        throw new Error('The specified value must be greater than or equal to' + propertySpec.min + '.');
+
+                if (typeof propertySpec.max != 'undefined')
+                    if (value > propertySpec.max)
+                        throw new Error('The specified value must be less than or equal to' + propertySpec.max + '.');
+
+                if (typeof propertySpec.values != 'undefined')
+                    if (propertySpec.values.indexOf(value) == -1)
+                        throw new Error('"' + value + '" is not an acceptable value.');
+
+                break;
+
+            case 'string':
+                if (typeof propertySpec.values != 'undefined')
+                    if (propertySpec.values.indexOf(value) == -1)
+                        throw new Error('"' + value + '" is not an acceptable value.');
+                break;
+        }
+
+        var propertyPath: string = FilePathConstructor.motorProperty(this.port, property);
+
+        if (fs.existsSync(propertyPath))
+            return fs.writeFileSync(propertyPath, value);
+        else
+            throw new Error('The property file could not be found. Either the specified motor is not available or the property does not exist.');
+    }
+
+    public run(options: motorRunOptions) {
+        for (var i in defaultMotorRunOptions)
+            if (options[i] == undefined)
+                options[i] = defaultMotorRunOptions[i];
+
+        this.writeProperty(MotorProperty.regulation_mode, softBoolean(options.regulationMode, 'off', 'on'));
+        this.writeProperty(MotorProperty.run, softBoolean(options.run,0,1));
+        this.writeProperty(MotorProperty.speed_setpoint, options.targetSpeed);
+    }
+
     constructor(port: MotorPort) {
         this.port = port;
     }
+}
+
+interface motorRunOptions {
+    targetSpeed: number //equates to speed_setpoint. Default: 0
+    run: any //will accept numbers 0 and 1, strings 'off' and 'on', and booleans true and false. Default: true
+    regulationMode: any //will accept numbers 0 and 1, strings 'off' and 'on', and booleans true and false. Default: false
+
+}
+
+var defaultMotorRunOptions = {
+    targetSpeed: 0,
+    run: 1,
+    regulationMode: false
 }
 
 module.exports = Motor;
